e3f6ab1d584e6416f58cde806f5dfcb8bf1a836d
yuragms <yura.gms@gmail.com>
2023-03-24 15:03:00 +0200

    6

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// const API = axios.create({
-//   baseURL: process.env.REACT_APP_PUBLIC_SERVER_HEROKKU,
-//   headers: {
-//     'Access-Control-Allow-Origin':
-//       'https://social-mediapp-mongo-noda3.herokuapp.com',
-//   },
-// });
 const API = axios.create({
-  baseURL: 'https://social-mediapp-mongo-noda3.herokuapp.com',
+  baseURL: process.env.REACT_APP_PUBLIC_SERVER_HEROKKU,
+  headers: {
+    'Access-Control-Allow-Origin':
+      'https://social-mediapp-mongo-noda3.herokuapp.com',
+  },
 });
+// const API = axios.create({
+//   baseURL: 'https://social-mediapp-mongo-noda3.herokuapp.com',
+// });
 export const logIn = (formData) => API.post('/auth/login', formData);
 export const signUp = (formData) => API.post('/auth/register', formData);
